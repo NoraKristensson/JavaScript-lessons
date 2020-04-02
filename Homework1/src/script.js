db5b2e6211bd652156cc307c1a3635f69ecb4263
“username”Norakristensson <email@email.comkristensson.nora@gmail.com>
2020-04-02 22:15:07 +0200

    first commit

---
+++ Homework1/src/script.js
@@ -0,0 +1,21 @@
+// assigment nr 1
+
+// let square = prompt("Type the length of the square");
+
+// perimeter = square * 4;
+
+// console.log(perimeter);
+
+// -----------------------------------------------
+
+// assigment nr 2
+let birthYear = prompt("when are you born?");
+const year = 2020;
+
+let age = year - birthYear;
+
+console.log(age);
+
+// Request a user’s year of birth, count their age,
+// and display the result to the console.
+// Store the current year as a constant.
