2c9aac3a9d5d95cc4db1fff33b171499179f9e42
Nora Kristensson <kristensson.nora@gmail.com>
2020-05-01 01:34:07 +0200

    homework done

---
+++ Homework/slides/src/scripts.js
@@ -0,0 +1,12 @@
+function myFunction() {
+	let x = document.getElementById('myTopnav');
+	if (x.className === 'links') {
+		x.className += ' responsive';
+	} else {
+		x.className = 'links';
+	}
+}
+
+$('.slider-container').slick({
+	dots: true,
+});
