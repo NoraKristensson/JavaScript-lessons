a0e1017ec19925364dbfd2efe5249b23769b81f7
“username”Norakristensson <email@email.comkristensson.nora@gmail.com>
2020-05-08 11:42:09 +0200

    classwork employee

---
+++ classwork/Employees list/exercise_1/src/script.js
@@ -0,0 +1,12 @@
+$.ajax("/data/employees.json").done((employees) => {
+  let list = $("<ul></ul>");
+  list.addClass("bulleted");
+
+  for (let employee of employees) {
+    let listItem = $("<li></li>");
+
+    listItem.text(employee.name).addClass(employee.inOffice ? "in" : "out");
+    list.append(listItem);
+  }
+  $("#employeeList").append(list);
+});
